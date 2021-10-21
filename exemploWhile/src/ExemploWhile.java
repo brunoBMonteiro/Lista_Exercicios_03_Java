0c089f2a6802ea90dbc5f2ec82180549f5703fdd
Bruno <brunobilheri@hotmail.com>
2021-10-21 08:42:49 -0300

    exemplo while, enquanto

---
+++ exemploWhile/src/ExemploWhile.java
@@ -0,0 +1,14 @@
+public class ExemploWhile {
+    public static void main(String[] args){
+
+        int i = 0;
+        int max = 10;
+
+        System.out.println("Concatenando até: " + max);
+
+        while(i < max){
+            i++;
+            System.out.println(i);
+        }
+    }
+}
